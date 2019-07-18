324e960184e96f62dcf40e775c9cf31d7cb0a27f
miyamoen <akihito.biblioteca@gmail.com>
2019-07-18 15:10:44 +0900

    コメント追加

--- src/KataKata01SameModel.elm
+++ src/KataKata01SameModel.elm
@@ -1,5 +1,29 @@
 module KataKata01SameModel exposing (Model, Msg(..), init, main, update, view)
 
+{-|
+
+
+# katakata 01 Same Model
+
+最初のkataです。まず一番簡単なフォームから始めてみましょう
+
+
+## ストーリー
+
+あなたは10文字以下の文字列が必要になりました。そこで`Model`に`text`という名前で保持することにしました。このtextは取り急ぎ画面上に表示しておこうと思います。
+textは変更できるようにしたいと思います。そこでテキスト入力フォームも実装することにしました
+
+表示の部分とtextが更新される部分を作りましたが、何か忘れています。テストで確認しましょう！
+
+
+## やり方
+
+  - elm reactorでこのファイルを開きましょう
+  - 画面下部に表示されるテスト結果を読んで`replace____me*`を書き換えましょう
+  - 🎉🎉テストが全部通ったらクリアです！🎉🎉
+
+-}
+
 import Browser
 import Element exposing (..)
 import Element.Input
@@ -11,10 +35,14 @@ import KataKata.Test as Test exposing (Test)
 import KataKata.Util exposing (replace____me)
 
 
+{-| textが今回欲しい10文字以下の文字列です
+-}
 type alias Model =
     { text : String }
 
 
+{-| textを変更するためのMsgです
+-}
 type Msg
     = ChangeText String
 
@@ -28,6 +56,7 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         ChangeText string ->
+            -- 何か実装し忘れているような
             { model | text = replace____me string }
 
 
