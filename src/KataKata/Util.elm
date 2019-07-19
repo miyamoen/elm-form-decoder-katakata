7fb4a387f2f5bfd6ed318a8e7310f40397a54040
miyamoen <akihito.biblioteca@gmail.com>
2019-07-19 11:25:28 +0900

    katakata 03 Int & Record

--- src/KataKata/Util.elm
+++ src/KataKata/Util.elm
@@ -1,6 +1,26 @@
-module KataKata.Util exposing (replace____me)
+module KataKata.Util exposing
+    ( replace____me
+    , replace____me1
+    , replace____me2
+    , replace____me3
+    )
 
 
 replace____me : a -> a
 replace____me a =
+    a
+
+
+replace____me1 : a -> a
+replace____me1 a =
+    a
+
+
+replace____me2 : a -> a
+replace____me2 a =
+    a
+
+
+replace____me3 : a -> a
+replace____me3 a =
     a
--- src/KataKata03IntAndRecord.elm
+++ src/KataKata03IntAndRecord.elm
@@ -10,10 +10,11 @@ Int型と合わせてRecord型にしよう。
 
 ## ストーリー
 
-あなたは前回のフォームで10文字以上入力できなくなっているのがおかしいと感じ始めました。初めて触ったひとには動かなくなって壊れたのかと思われてしまいそうです。それに勝手に文字列を切り取ってしまうのはよくないでしょう。
-そこで、入力欄のModelはtextとは別のものにして、都度変換することにしました。変換は失敗するかもしれないのでtextをMaybeにしました。
+あなたは10文字以下の文字列のほかに新たに1以上10以下の整数も必要になりました。個人的にスライダーUIは使いにくいしコピペで入力できないのでテキスト入力で入力することにしました。
+textとnumberをまとめてレコード型で扱うことにしました。フォーム用のレコードと変換した後のレコードの2つをModelに入れました。フォームからの変換はいろいろな理由で失敗しそうなので`Result String Value`にしました。
+また、忘れていましたが空文字のtextは意味がないのでこれもちゃんとチェックしておこうと思います。
 
-Modelの定義と変換するMsgも作りましたが、何か忘れています。テストで確認しましょう！
+また肝心の変換を実装し忘れています。テストで確認しましょう！
 
 
 ## やり方
@@ -63,15 +64,18 @@ init =
 
 
 update : Msg -> Model -> Model
-update msg model =
+update msg ({ form } as model) =
     case msg of
         ChangeText string ->
-            { model | formText = string }
+            { model | form = { form | text = string } }
+
+        ChangeNumber string ->
+            { model | form = { form | number = string } }
 
         ConvertForm ->
             { model
-              -- `replace____me model.text`全体を書き換えてください
-                | text = replace____me model.text
+              -- `replace____me model.value`全体を書き換えてください
+                | value = replace____me model.value
             }
 
 
@@ -84,9 +88,15 @@ view model =
                 , htmlAttribute <| Html.Attributes.autofocus True
                 ]
                 { onChange = ChangeText
-                , text = model.formText
+                , text = model.form.text
                 , placeholder = Nothing
-                , label = Element.Input.labelHidden "text"
+                , label = Element.Input.labelAbove [] <| text "text"
+                }
+            , Element.Input.text [ width fill ]
+                { onChange = ChangeNumber
+                , text = model.form.number
+                , placeholder = Nothing
+                , label = Element.Input.labelAbove [] <| text "number"
                 }
             , row [ width fill, spacing 16 ]
                 [ Element.Input.button
@@ -94,16 +104,20 @@ view model =
                     , Border.rounded 4
                     , Border.color <| rgba255 0 0 0 0.3
                     , Background.color <| rgb255 255 236 165
+                    , alignTop
                     ]
                     { onPress = Just ConvertForm
                     , label = el [ padding 8 ] <| text "変換する"
                     }
-                , case model.text of
-                    Just string ->
-                        wrappedText [ width fill ] string
+                , case model.value of
+                    Ok value ->
+                        column [ width fill, spacing 16 ]
+                            [ wrappedText [ width fill ] value.text
+                            , text <| String.fromInt value.number
+                            ]
 
-                    Nothing ->
-                        text "入力欄が10文字より長いです"
+                    Err error ->
+                        text error
                 ]
             ]
 
@@ -116,28 +130,75 @@ main =
 testSuite : Test
 testSuite =
     Test.describe "03 - Int & Record"
-        [ Test.test "textはformTextから変換されます" <|
+        [ Test.test "valueはformから変換されます" <|
             \_ ->
                 let
-                    text =
-                        "虎になる"
+                    value =
+                        { text = "虎になる"
+                        , number = 5
+                        }
                 in
                 init
-                    |> update (ChangeText text)
+                    |> update (ChangeText value.text)
+                    |> update (ChangeNumber <| String.fromInt value.number)
                     |> update ConvertForm
-                    |> .text
-                    |> Expect.equal (Just text)
-                    |> Expect.onFail "textがformTextから変換されるように`replace____me`を書き換えましょう"
-        , Test.test "formTextが10文字より長いとき、変換は失敗します" <|
+                    |> .value
+                    |> Expect.equal (Ok value)
+                    |> Expect.onFail "valueがformから変換されるように`replace____me`を書き換えましょう"
+        , Test.test "form.textが10文字より長いとき、変換は失敗します" <|
+            \_ ->
+                init
+                    |> update (ChangeText "我が臆病な自尊心と、尊大な羞恥心との所為である")
+                    |> update (ChangeNumber "5")
+                    |> update ConvertForm
+                    |> .value
+                    |> Expect.err
+                    |> Expect.onFail "form.textが10文字以上のときは変換が失敗するようにしましょう"
+        , Test.test "form.textが空文字のとき、変換は失敗します" <|
+            \_ ->
+                init
+                    |> update (ChangeText "")
+                    |> update (ChangeNumber "5")
+                    |> update ConvertForm
+                    |> .value
+                    |> Expect.err
+                    |> Expect.onFail "form.textが空文字のときは変換が失敗するようにしましょう"
+        , Test.test "form.numberが整数に変換できなければ、変換は失敗します" <|
+            \_ ->
+                init
+                    |> update (ChangeText "虎になる")
+                    |> update (ChangeNumber "数字のつもり")
+                    |> update ConvertForm
+                    |> .value
+                    |> Expect.err
+                    |> Expect.onFail "form.numberが整数にできないときは変換が失敗するようにしましょう"
+        , Test.test "form.numberが1以上10以下の整数に変換できなければ、変換は失敗します" <|
             \_ ->
                 let
-                    text =
-                        "我が臆病な自尊心と、尊大な羞恥心との所為である"
+                    zero =
+                        init
+                            |> update (ChangeText "虎になる")
+                            |> update (ChangeNumber "0")
+                            |> update ConvertForm
+                            |> .value
+
+                    eleven =
+                        init
+                            |> update (ChangeText "虎になる")
+                            |> update (ChangeNumber "11")
+                            |> update ConvertForm
+                            |> .value
                 in
-                init
-                    |> update (ChangeText text)
-                    |> update ConvertForm
-                    |> .text
-                    |> Expect.equal Nothing
-                    |> Expect.onFail "formTextからtextへの変換をformTextが10文字より長いときは失敗するようにしましょう"
+                case ( zero, eleven ) of
+                    ( Err _, Err _ ) ->
+                        Expect.pass
+
+                    ( Err _, Ok _ ) ->
+                        Expect.fail "10以下の条件を忘れていませんか？"
+
+                    ( Ok _, Err _ ) ->
+                        Expect.fail "1以上の条件を忘れていませんか？"
+
+                    _ ->
+                        Expect.fail "form.numberが1以上10以下の整数に変換できなければ失敗するようにしましょう"
         ]
